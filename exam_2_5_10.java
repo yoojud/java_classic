5439f07f75f41c86ee90016583d59eb84bd48753
yoojud <youjud@naver.com>
2025-01-07 20:16:18 +0900

    update

--- exam_2_5_10.java
+++ exam_2_5_10.java
@@ -1,3 +1,7 @@
 public class exam_2_5_10 {
-    
+    public static void main(String[] args) {
+        System.out.printf("(5 + 3) * 2 = %d%n", (5 + 3) * 2);
+        System.out.printf("5 + 3 * 2 = %d%n", 5 + 3 * 2);
+        System.out.printf("(10 / 2) * (3 + 1) = %d%n", (10 / 2) * (3 + 1));
+    }
 }
